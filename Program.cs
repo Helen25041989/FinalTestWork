c0934cd5b3ee0dbb696750c4db158bd11b550bb3
Sergo <TEW6rfHz0>
2022-10-01 15:27:53 +0300

    create program

--- Program.cs
+++ Program.cs
@@ -5,5 +5,3 @@ Console.WriteLine("Исходный массив= {0}", string.Join(", ",massiv)
 string [] result = massiv.Where(x => x.Length <= 3).ToArray();
 
 Console.WriteLine("Результирущий массив= {0}", string.Join(", ", result));
-
-
