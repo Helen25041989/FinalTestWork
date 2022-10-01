c60a397474b421edb98250d2becf14f3305ba6c1
Sergo <TEW6rfHz0>
2022-10-01 15:27:34 +0300

    create program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] massiv = new string[]{"hello","qwerty","2",":-("};
+
+Console.WriteLine("Исходный массив= {0}", string.Join(", ",massiv));
+
+string [] result = massiv.Where(x => x.Length <= 3).ToArray();
+
+Console.WriteLine("Результирущий массив= {0}", string.Join(", ", result));
+
+
